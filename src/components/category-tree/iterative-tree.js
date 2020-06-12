27599f5642824a4474ac974b9517a61817258a78
Laimonas Jočys <laimonas.jocys@geniussports.com>
2020-06-12 13:15:52 +0300

    feat: add iterative tree

--- src/components/category-tree/iterative-tree.js
+++ src/components/category-tree/iterative-tree.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import TreeItem from '@material-ui/lab/TreeItem';
+import {v4 as uuid} from 'uuid';
 
 const IterativeTree = ({categories}) => {
-  return null
+  const render = (category) => {
+    return (
+      <TreeItem label={category.label} nodeId={uuid()}>
+        {category.children && category.children.map(render)}
+      </TreeItem>
+    )
+  }
+
+  return categories.map((category, index) => {
+    return (
+      <React.Fragment key={index}>
+        {render(category)}
+      </React.Fragment>
+    )
+  })
 }
 
 export default IterativeTree;
