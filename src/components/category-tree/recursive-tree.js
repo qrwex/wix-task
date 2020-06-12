b8926a1c3a421ca0112e105051aa680523bd224a
Laimonas Jočys <laimonas.jocys@geniussports.com>
2020-06-12 13:03:10 +0300

    chore: code reformat

--- src/components/category-tree/recursive-tree.js
+++ src/components/category-tree/recursive-tree.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import { v4 as uuid } from 'uuid'
+import {v4 as uuid} from 'uuid'
 import TreeItem from '@material-ui/lab/TreeItem';
 
-const RecursiveTree = ({categories}) => {
-
-  return categories.map(category => {
-    const id = uuid();
-
-    return (
-      <TreeItem key={id} label={category.label} nodeId={id} >
+const RecursiveTree = ({categories}) => (
+  categories.map((category, index) => (
+      <TreeItem key={index} label={category.label} nodeId={uuid()}>
         {category.children && (
-          <RecursiveTree categories={category.children} />
+          <RecursiveTree categories={category.children}/>
         )}
       </TreeItem>
-    );
-  })
-}
+    )
+  )
+);
 
 export default RecursiveTree;
